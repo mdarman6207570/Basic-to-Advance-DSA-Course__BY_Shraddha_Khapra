2a8200d7be1018832ec40dfcbf23bccd5b03ef83
mdarman6207570 <mdarman6207570@gmail.com>
2025-08-16 15:17:03 +0530

    as

---
+++ 03__adult or not/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class conditions {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextint();
+
+        if (age > 18)
+            System.out.println(" adult");
+
+    }
+    else{
+        System.out.println("not adult");
+
+    }
+}
